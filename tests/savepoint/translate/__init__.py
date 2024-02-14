a4470be74f3cfa6ac7b3b91c7f1b1fa54c0b142f
Frank Malatino <frank.malatino@noaa.gov>
2024-02-14 10:38:11 -0500

    Updated __init__ files after lint

--- tests/savepoint/translate/__init__.py
+++ tests/savepoint/translate/__init__.py
@@ -2,10 +2,8 @@
 from .translate_atmos_phy_statein import TranslateAtmosPhysDriverStatein
 from .translate_fillgfs import TranslateFillGFS
 from .translate_fv_update_phys import TranslateFVUpdatePhys
-#from .translate_gfs_physics_driver import TranslateGFSPhysicsDriver
 from .translate_microphysics import TranslateMicroph
 from .translate_phifv3 import TranslatePhiFV3
-from .translate_physics import ParallelPhysicsTranslate2Py, TranslateFortranData2Py
 from .translate_prsfv3 import TranslatePrsFV3
 from .translate_update_dwind_phys import TranslateUpdateDWindsPhys
 from .translate_update_pressure_sfc_winds_phys import (
