33f2a6cecf572f54edfcbc46d37daa64363812ac
Frank Malatino <142349306+fmalatino@users.noreply.github.com>
2024-02-20 12:17:38 -0500

    Update pySHiELD/update/__init__.py
    Co-authored-by: Oliver Elbert <oliver.elbert36@gmail.com>

--- pySHiELD/update/__init__.py
+++ pySHiELD/update/__init__.py
@@ -4,12 +4,8 @@ from .update_dwind_phys import AGrid2DGridPhysics
 
 
 """
-ApplyPhysicsToDycore: Class containing methods to apply physics to FV3
-                      discretiztion and prognostic variables
-DycoreToPhysics: Class containing methods for transferring dynamical
-                 core state information to physics state
-UpdateAtmosphereState: Class containing methods for applying tendencies
-                       and keeping prognostic state consistent
-AGrid2DGridPhysics: Class containing methods to update D-grid wind
-                    physics from A-grid state
+ApplyPhysicsToDycore: Class to update the pyFV3 dynamical core state from the physics state
+DycoreToPhysics: Class to set the physics state from the pyFV3 dynamical core state
+UpdateAtmosphereState: Class containing methods to apply tendencies while keeping prognostic state consistent
+AGrid2DGridPhysics: Class to transform wind tendencies from the A-grid to the D grid
 """
