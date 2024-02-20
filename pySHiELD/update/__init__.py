a4f0d846b5ca278689675ae5c7917fb28f0240b9
Frank Malatino <frank.malatino@noaa.gov>
2024-02-20 13:11:41 -0500

    Fixing linting issues

--- pySHiELD/update/__init__.py
+++ pySHiELD/update/__init__.py
@@ -4,8 +4,10 @@ from .update_dwind_phys import AGrid2DGridPhysics
 
 
 """
-ApplyPhysicsToDycore: Class to update the pyFV3 dynamical core state from the physics state
+ApplyPhysicsToDycore: Class to update the pyFV3 dynamical core state from the physics
+                      state
 DycoreToPhysics: Class to set the physics state from the pyFV3 dynamical core state
-UpdateAtmosphereState: Class containing methods to apply tendencies while keeping prognostic state consistent
+UpdateAtmosphereState: Class containing methods to apply tendencies while keeping
+                       prognostic state consistent
 AGrid2DGridPhysics: Class to transform wind tendencies from the A-grid to the D grid
 """
