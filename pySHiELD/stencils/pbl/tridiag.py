a2ac226fcb0f737f32ebda79e44526699c45ed6b
Oliver Elbert <Oliver.Elbert@noaa.gov>
2024-10-30 12:34:03 -0400

    fixing error in tridiags

--- pySHiELD/stencils/pbl/tridiag.py
+++ pySHiELD/stencils/pbl/tridiag.py
@@ -22,8 +22,8 @@ def tridit(
 
     with computation(BACKWARD):
         with interval(-1, None):
-            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, -1] * au[0, 0, -1])
-            f1 = fk * (f1[0, 0, 0] - cl[0, 0, -1] * f1[0, 0, -1])
+            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, 0] * au[0, 0, -1])
+            f1 = fk * (f1[0, 0, 0] - cl[0, 0, 0] * f1[0, 0, -1])
         with interval(0, -1):
             f1 = f1[0, 0, 0] - au[0, 0, 0] * f1[0, 0, 1]
 
@@ -39,22 +39,21 @@ def tridi2(
     r2: FloatFieldTracer,
 ):
 
-    with computation(PARALLEL), interval(0, 1):
-        fk = 1 / cm[0, 0, 0]
-        au = fk * cu[0, 0, 0]
-        a1 = fk * r1[0, 0, 0]
-        a2[0, 0, 0][0] = fk * r2[0, 0, 0][0]
-
     with computation(FORWARD):
-        with interval(1, -1):
-            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, -1] * au[0, 0, -1])
+        with interval(0, 1):
+            fk = 1 / cm[0, 0, 0]
             au = fk * cu[0, 0, 0]
-            a1 = fk * (r1[0, 0, 0] - cl[0, 0, -1] * a1[0, 0, -1])
-            a2[0, 0, 0][0] = fk * (r2[0, 0, 0][0] - cl[0, 0, -1] * a2[0, 0, -1][0])
+            a1 = fk * r1[0, 0, 0]
+            a2[0, 0, 0][0] = fk * r2[0, 0, 0][0]
+        with interval(1, -1):
+            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, 0] * au[0, 0, -1])
+            au = fk * cu[0, 0, 0]
+            a1 = fk * (r1[0, 0, 0] - cl[0, 0, 0] * a1[0, 0, -1])
+            a2[0, 0, 0][0] = fk * (r2[0, 0, 0][0] - cl[0, 0, 0] * a2[0, 0, -1][0])
         with interval(-1, None):
-            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, -1] * au[0, 0, -1])
-            a1 = fk * (r1[0, 0, 0] - cl[0, 0, -1] * a1[0, 0, -1])
-            a2[0, 0, 0][0] = fk * (r2[0, 0, 0][0] - cl[0, 0, -1] * a2[0, 0, -1][0])
+            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, 0] * au[0, 0, -1])
+            a1 = fk * (r1[0, 0, 0] - cl[0, 0, 0] * a1[0, 0, -1])
+            a2[0, 0, 0][0] = fk * (r2[0, 0, 0][0] - cl[0, 0, 0] * a2[0, 0, -1][0])
 
     with computation(BACKWARD), interval(0, -1):
         a1 = a1[0, 0, 0] - au[0, 0, 0] * a1[0, 0, 1]
@@ -80,20 +79,20 @@ def tridin(
             a2[0, 0, 0][n_tracer] = fk * r2[0, 0, 0][n_tracer]
 
         with interval(1, -1):
-            fkk = 1.0 / (cm[0, 0, 0] - cl[0, 0, -1] * au[0, 0, -1])
+            fkk = 1.0 / (cm[0, 0, 0] - cl[0, 0, 0] * au[0, 0, -1])
             au = fkk * cu[0, 0, 0]
-            a1 = fkk * (r1[0, 0, 0] - cl[0, 0, -1] * a1[0, 0, -1])
+            a1 = fkk * (r1[0, 0, 0] - cl[0, 0, 0] * a1[0, 0, -1])
 
             a2[0, 0, 0][n_tracer] = fkk * (
-                r2[0, 0, 0][n_tracer] - cl[0, 0, -1] * a2[0, 0, -1][n_tracer]
+                r2[0, 0, 0][n_tracer] - cl[0, 0, 0] * a2[0, 0, -1][n_tracer]
             )
 
         with interval(-1, None):
-            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, -1] * au[0, 0, -1])
-            a1 = fk * (r1[0, 0, 0] - cl[0, 0, -1] * a1[0, 0, -1])
+            fk = 1.0 / (cm[0, 0, 0] - cl[0, 0, 0] * au[0, 0, -1])
+            a1 = fk * (r1[0, 0, 0] - cl[0, 0, 0] * a1[0, 0, -1])
 
             a2[0, 0, 0][n_tracer] = fk * (
-                r2[0, 0, 0][n_tracer] - cl[0, 0, -1] * a2[0, 0, -1][n_tracer]
+                r2[0, 0, 0][n_tracer] - cl[0, 0, 0] * a2[0, 0, -1][n_tracer]
             )
 
     with computation(BACKWARD):
