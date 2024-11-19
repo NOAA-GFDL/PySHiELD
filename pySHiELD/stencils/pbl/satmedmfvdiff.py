428f20ea038f9e72628f606491218bd9e542e23b
Oliver Elbert <Oliver.Elbert@noaa.gov>
2024-11-19 16:18:30 -0500

    init var

--- pySHiELD/stencils/pbl/satmedmfvdiff.py
+++ pySHiELD/stencils/pbl/satmedmfvdiff.py
@@ -165,11 +165,13 @@ def init_turbulence(
             rdzt = 1.0 / (zl[0, 0, 1] - zl[0, 0, 0])
         with interval(-2, -1):
             zm = zi[0, 0, 1]
-            kx1 = 0.0
     with computation(FORWARD), interval(0, 1):
         #  set background diffusivities as a function of
         #  horizontal grid size with xkzm_h & xkzm_m for gdx >= 25km
         #  and 0.01 for gdx=5m
+        kx1 = 0.0
+        tx1 = 1.0 / prsi
+        tx2 = tx1
         if gdx[0, 0] >= physcons.XKGDX:
             xkzm_hx = xkzm_h
             xkzm_mx = xkzm_m
@@ -181,6 +183,8 @@ def init_turbulence(
                 gdx[0, 0] - 5.0
             )
     with computation(FORWARD), interval(0, -2):
+        xkzo(i,k)  = 0.0
+        xkzmo(i,k) = 0.0
         if k_mask[0] < kinver[0, 0]:
             ptem = prsi[0, 0, 1] * tx1[0, 0]
             xkzo = xkzm_hx * min(1.0, exp(-((1.0 - ptem) * (1.0 - ptem) * 10.0)))
