9d5a5a039cfaf3dbfbc1f33c8616287da6ad74f9
Oliver Elbert <Oliver.Elbert@noaa.gov>
2024-11-21 16:48:48 -0500

    blep

--- pySHiELD/stencils/pbl/satmedmfvdiff.py
+++ pySHiELD/stencils/pbl/satmedmfvdiff.py
@@ -134,9 +134,9 @@ def init_turbulence(
         dvsfc = 0.0
         dtsfc = 0.0
         dqsfc = 0.0
-        kpbl = 2
+        kpbl = 0
         hpbl = 0.0
-        kpblx = 2
+        kpblx = 0
         hpblx = 0.0
         pblflg = True
         lcld = km1 - 1
