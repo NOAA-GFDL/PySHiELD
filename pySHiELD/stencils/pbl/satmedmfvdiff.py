c0c9eba6ac60b37944cb4122c971efee96dd8429
Oliver Elbert <Oliver.Elbert@noaa.gov>
2024-11-27 11:23:03 -0500

    k issue

--- pySHiELD/stencils/pbl/satmedmfvdiff.py
+++ pySHiELD/stencils/pbl/satmedmfvdiff.py
@@ -1132,7 +1132,10 @@ def tke_tridiag_matrix_ele_comp(
                 f1_p1 = tke[0, 0, 1] - (ptem2 - tem) * (dtodsu * ptem)
         with interval(1, -1):
             ad = ad_p1[0, 0]
-            f1 = f1_p1[0, 0]
+            if pcnvflg[0, 0] and k_mask[-1] < kpbl[0, 0]:
+                f1 = f1_p1[0, 0]
+            if scuflg[0, 0] and k_mask[-1] >= mrad[0, 0] and k_mask[-1] < krad[0, 0]:
+                f1 = f1_p1[0, 0]
 
             dtodsd = dt2 / delta[0, 0, 0]
             dtodsu = dt2 / delta[0, 0, 1]
@@ -1163,7 +1166,10 @@ def tke_tridiag_matrix_ele_comp(
 
         with interval(-1, None):
             ad = ad_p1[0, 0]
-            f1 = f1_p1[0, 0]
+            if pcnvflg[0, 0] and k_mask[-1] < kpbl[0, 0]:
+                f1 = f1_p1[0, 0]
+            if scuflg[0, 0] and k_mask[-1] >= mrad[0, 0] and k_mask[-1] < krad[0, 0]:
+                f1 = f1_p1[0, 0]
     with computation(PARALLEL), interval(...):
         cu = au
         rt = f1
@@ -1282,9 +1288,13 @@ def heat_moist_tridiag_mat_ele_comp(
                 f2[0, 0, 0][0] = f2[0, 0, 0][0] + tem * ptem1
                 f2_p1 = f2_p1[0, 0] - tem * ptem2
         with interval(1, -1):
-            f1 = f1_p1[0, 0]
-            f2[0, 0, 0][0] = f2_p1[0, 0]
             ad = ad_p1[0, 0]
+            if pcnvflg[0, 0] and k_mask[-1] < kpbl[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
+            if scuflg[0, 0] and k_mask[-1] >= mrad[0, 0] and k_mask[-1] < krad[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
 
             dtodsd = dt2 / delta[0, 0, 0]
             dtodsu = dt2 / delta[0, 0, 1]
@@ -1332,9 +1342,13 @@ def heat_moist_tridiag_mat_ele_comp(
                 f2[0, 0, 0][0] = f2[0, 0, 0][0] + tem * ptem1
                 f2_p1 = f2_p1[0, 0] - tem * ptem2
         with interval(-1, None):
-            f1 = f1_p1[0, 0]
-            f2[0, 0, 0][0] = f2_p1[0, 0]
             ad = ad_p1[0, 0]
+            if pcnvflg[0, 0] and k_mask[-1] < kpbl[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
+            if scuflg[0, 0] and k_mask[-1] >= mrad[0, 0] and k_mask[-1] < krad[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
 
     with computation(PARALLEL), interval(...):
         cu = (au,)
@@ -1570,8 +1584,12 @@ def moment_tridiag_mat_ele_comp(
                 f2[0, 0, 0][0] = f2[0, 0, 0][0] + tem * ptem1
                 f2_p1 = f2_p1[0, 0] - tem * ptem2
         with interval(1, -1):
-            f1 = f1_p1[0, 0]
-            f2[0, 0, 0][0] = f2_p1[0, 0]
+            if pcnvflg[0, 0] and k_mask[-1] < kpbl[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
+            if scuflg[0, 0] and k_mask[-1] >= mrad[0, 0] and k_mask[-1] < krad[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
             ad = ad_p1[0, 0]
 
             dtodsd = dt2 / delta[0, 0, 0]
@@ -1610,8 +1628,12 @@ def moment_tridiag_mat_ele_comp(
                 f2_p1 = f2_p1[0, 0] - tem * ptem2
 
         with interval(-1, None):
-            f1 = f1_p1[0, 0]
-            f2[0, 0, 0][0] = f2_p1[0, 0]
+            if pcnvflg[0, 0] and k_mask[-1] < kpbl[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
+            if scuflg[0, 0] and k_mask[-1] >= mrad[0, 0] and k_mask[-1] < krad[0, 0]:
+                f1 = f1_p1[0, 0]
+                f2[0, 0, 0][0] = f2_p1[0, 0]
             ad = ad_p1[0, 0]
     with computation(PARALLEL), interval(...):
         cu = au
