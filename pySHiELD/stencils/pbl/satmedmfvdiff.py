988f9a947a84e25c99c203d6e98f040fe7ecfb06
Oliver Elbert <Oliver.Elbert@noaa.gov>
2024-12-18 15:09:18 -0500

    isk

--- pySHiELD/stencils/pbl/satmedmfvdiff.py
+++ pySHiELD/stencils/pbl/satmedmfvdiff.py
@@ -220,7 +220,7 @@ def init_turbulence(
         xkzmo[0, 0, 0] = 0.0
         if k_mask[0] < kinver[0, 0]:
             # vertical background diffusivity
-            ptem = prsi[0, 0, 1] * tx1[0, 0]
+            ptem = prsi[0, 0, 1] / prsi[0, 0, 0]
             tem1 = (1.0 - ptem) * (1.0 - ptem) * 10.0
             xkzo[0, 0, 0] = xkzm_hx * min(1.0, exp(-tem1))
             # vertical background diffusivity for momentum
