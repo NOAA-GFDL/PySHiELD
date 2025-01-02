77ea0a435e6f2898f7de1686e875e1dc528e22e5
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-01-02 12:26:10 -0500

    oops

--- tests/savepoint/translate/translate_pbl_subtests.py
+++ tests/savepoint/translate/translate_pbl_subtests.py
@@ -82,7 +82,7 @@ class InitTurb:
         self._tem1 = quantity_factory.zeros(
             [X_DIM, Y_DIM, Z_DIM],
             units="unknown",
-            dtype=Int,
+            dtype=Float,
         )
 
         for k in range(idx.domain[2]):
