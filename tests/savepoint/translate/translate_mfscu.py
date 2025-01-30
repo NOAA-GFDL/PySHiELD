d9f405d40edd437de4a04a92c3c8689b38282701
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-01-30 15:39:33 -0500

    mrad?

--- tests/savepoint/translate/translate_mfscu.py
+++ tests/savepoint/translate/translate_mfscu.py
@@ -86,11 +86,6 @@ class TranslateMFSCU(TranslatePhysicsFortranData2Py):
             dims=[X_DIM, Y_DIM],
             units="",
         )
-        mrad = quantity_factory.from_array(
-            data=inputs.pop("mrad"),
-            dims=[X_DIM, Y_DIM],
-            units="",
-        )
         zm = quantity_factory.from_array(
             data=inputs.pop("zm"),
             dims=[X_DIM, Y_DIM, Z_DIM],
@@ -107,6 +102,6 @@ class TranslateMFSCU(TranslatePhysicsFortranData2Py):
             ntke=8,
         )
 
-        compute_func(**inputs, k_mask=k_mask, cnvflg=cnvflg, mrad=mrad, zm=zm)
+        compute_func(**inputs, k_mask=k_mask, cnvflg=cnvflg, zm=zm)
 
         return self.slice_output(inputs)
