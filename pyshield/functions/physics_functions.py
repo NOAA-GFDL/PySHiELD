a7e0daa1be98d217b40cd55fb71d1fb9994678eb
Frank Malatino <frank.malatino@noaa.gov>
2025-07-15 14:38:37 -0400

    Updating tools to match pyFV3 linting and versions

--- pyshield/functions/physics_functions.py
+++ pyshield/functions/physics_functions.py
@@ -58,13 +58,13 @@ def fpvsx(t):
 
     fpvsx = 0.0
     if t >= tliq:
-        fpvsx = constants.PSAT * (tr ** xponal) * exp(xponbl * (1.0 - tr))
+        fpvsx = constants.PSAT * (tr**xponal) * exp(xponbl * (1.0 - tr))
     elif t < tice:
-        fpvsx = constants.PSAT * (tr ** xponai) * exp(xponbi * (1.0 - tr))
+        fpvsx = constants.PSAT * (tr**xponai) * exp(xponbi * (1.0 - tr))
     else:
         w = (t - tice) / (tliq - tice)
-        pvl = constants.PSAT * (tr ** xponal) * exp(xponbl * (1.0 - tr))
-        pvi = constants.PSAT * (tr ** xponai) * exp(xponbi * (1.0 - tr))
+        pvl = constants.PSAT * (tr**xponal) * exp(xponbl * (1.0 - tr))
+        pvi = constants.PSAT * (tr**xponai) * exp(xponbi * (1.0 - tr))
         fpvsx = w * pvl + (1.0 - w) * pvi
 
     return fpvsx
