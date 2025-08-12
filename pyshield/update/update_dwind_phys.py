c32af7f3a051b791a392993eb889263cdda423e1
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-08-12 15:41:38 -0400

    orch dace

--- pyshield/update/update_dwind_phys.py
+++ pyshield/update/update_dwind_phys.py
@@ -2,7 +2,7 @@ from gt4py.cartesian.gtscript import PARALLEL, computation, interval
 
 from ndsl.comm.partitioner import TilePartitioner
 from ndsl.constants import X_DIM, Y_DIM, Z_DIM
-from ndsl.dsl.dace import orchestrate
+from ndsl.dsl.dace.orchestration import orchestrate
 from ndsl.dsl.stencil import StencilFactory
 from ndsl.dsl.typing import FloatField, FloatFieldI, FloatFieldIJ
 from ndsl.grid import DriverGridData
