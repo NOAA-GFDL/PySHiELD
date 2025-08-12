c3d03745129a9124dd569b23064c3e8ffbcdd971
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-08-12 15:33:12 -0400

    oops

--- pyshield/stencils/shield_microphysics/physical_functions.py
+++ pyshield/stencils/shield_microphysics/physical_functions.py
@@ -2,7 +2,7 @@ from gt4py.cartesian import gtscript
 from gt4py.cartesian.gtscript import exp, floor, log, sqrt
 
 import ndsl.stencils.basic_operations as basic
-import pyshield.constants as constants
+import ndsl.constants as constants
 import pyshield.constants as physcons
 
 
