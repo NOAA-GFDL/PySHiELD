214a965a92fe3b4bb9893c8fb5606cece3ca830d
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-08-20 12:16:57 -0400

    add init

---
+++ pyshield/stencils/surface/__init__.py
@@ -0,0 +1,10 @@
+from .surface_layer import SurfaceLayer
+from .sfc_state import SurfaceState
+
+
+"""
+SurfaceLayer: Surface physics class
+SurfaceState: Class containing the state for the surface parameters
+"""
+
+__all__ = ["SurfaceLayer", "SurfaceState"]
