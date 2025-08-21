7ae67598383e30b64aa4ed699535aec5b7cab280
Oliver Elbert <oliver.elbert36@gmail.com>
2025-08-21 17:01:42 -0400

    init

--- pyshield/radiation/__init__.py
+++ pyshield/radiation/__init__.py
@@ -1,2 +1,11 @@
 from .radiation_state import RadiationState
 from .rte_rrtmgp import RadiationConfig, RTE_RRTMGPDriver
+
+
+"""
+RadiationConfig: Configuration class for the radiation driver
+RTE_RRTMGPDriver: Driver for the RTE_RRTMGP code
+RadiationState: Class containing the state for the RTE-RRTMGP radiation code
+"""
+
+__all__ = ["RTE_RRTMGPDriver", "RadiationConfig", "RadiationState"]
