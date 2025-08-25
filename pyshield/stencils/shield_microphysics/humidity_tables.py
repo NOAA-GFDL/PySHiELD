7e8391001869c17c9c6bf07d3adc23e58bdd39d2
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-08-25 16:03:54 -0400

    who knows

--- pyshield/stencils/shield_microphysics/humidity_tables.py
+++ pyshield/stencils/shield_microphysics/humidity_tables.py
@@ -21,6 +21,65 @@ class HumiditySaturationTables:
         self._initialize_table0()
         self._initialize_table2()
 
+    def qs_table_core(
+        self,
+        n: int,
+        n_blend: int,
+        do_smith_table: bool
+    ):
+        esupc = np.zeros(n_blend)
+        esbasw = 1013246.0
+        tbasw = physcons.TICE0 + 100.
+        esbasi = 6107.1
+        tmin = physcons.TICE0 - self.n_min * self.delt
+        # compute es over ice between - (n_min * delt) deg C and 0 deg C
+        if do_smith_table:
+            for i in range(self.n_min):
+                tem = tmin + self.delt * float(i)
+                a = - 9.09718 * (physcons.TICE0 / tem - 1.)
+                b = - 3.56654 * np.log10(physcons.TICE0 / tem)
+                c = 0.876793 * (1. - tem / physcons.TICE0)
+                e = np.log10(esbasi)
+                self.table2[i] = 0.1 * np.exp((a + b + c + e) * np.log(10.))
+        else:
+            for i in range(self.n_min):
+                tem = tmin + self.delt * float(i)
+                fac0 = (tem - physcons.TICE0) / (tem * physcons.TICE0)
+                fac1 = fac0 * physcons.LI2
+                fac2 = (physcons.D2ICE * np.log(tem / physcons.TICE0) + fac1) / constants.RVGAS
+                self.table2[i] = constants.E00 * np.exp(fac2)
+        # ! compute es over water between - (n_blend * delt) deg C and [ (n - n_min - 1) * delt] deg C
+        if do_smith_table:
+            for i in range(n - self.n_min + n_blend):
+                tem = physcons.TICE0 + self.delt * (float(i) - n_blend)
+                a = - 7.90298 * (tbasw / tem - 1.)
+                b = 5.02808 * np.log10(tbasw / tem)
+                c = - 1.3816e-7 * (np.exp((1. - tem / tbasw) * 11.344 * np.log(10.)) - 1.)
+                d = 8.1328e-3 * (np.exp((tbasw / tem - 1.) * (- 3.49149) * np.log(10.)) - 1.)
+                e = np.log10(esbasw)
+                esh = 0.1 * np.exp((a + b + c + d + e) * np.log(10.))
+            if i < n_blend:
+                esupc[i] = esh
+            else:
+                self.table2[i + self.n_min - n_blend] = esh
+        else:
+            for i in range(n - self.n_min + n_blend):
+                tem = physcons.TICE0 + self.delt * (float(i) - n_blend)
+                fac0 = (tem - physcons.TICE0) / (tem * physcons.TICE0)
+                fac1 = fac0 * physcons.LV0
+                fac2 = (physcons.DC_VAP * np.log(tem / physcons.TICE0) + fac1) / constants.RVGAS
+                esh = constants.E00 * np.exp(fac2)
+                if i < n_blend:
+                    esupc[i] = esh
+                else:
+                    self.table2[i + self.n_min - n_blend] = esh
+        # derive blended es over ice and supercooled water between - (n_blend * delt) deg C and 0 deg C
+        for i in range(n_blend):
+            tem = physcons.TICE0 + self.delt * (float(i) - n_blend)
+            wice = 1.0 / (self.delt * n_blend) * (physcons.TICE0 - tem)
+            wh2o = 1.0 / (self.delt * n_blend) * (tem - physcons.TICE0 + self.delt * n_blend)
+            self.table2[i + self.n_min - n_blend] = wice * self.table2[i + self.n_min - n_blend] + wh2o * esupc[i]
+
     def _initialize_table0(self):
         # TODO: numpy-ify these init methods?
         tmin = physcons.TICE0 - 160.0
@@ -38,6 +97,17 @@ class HumiditySaturationTables:
 
         self.des0[-1] = self.des0[-2]
         self._t0_init = True
+
+    def _init_table2_qs(self):
+        if not self._t0_init:
+            self._initialize_table0
+        self.qs_table_core(self.length, 0, False)
+        for i in range(self.length):
+            if i > 0:
+                self.des2[i - 1] = max(0.0, self.table2[i] - self.table2[i - 1])
+
+        self.des2[-1] = self.des2[-2]
+        self._t2_init = True
 
     def _initialize_table2(self):
         if not self._t0_init:
@@ -74,7 +144,7 @@ class HumiditySaturationTables:
             raise ValueError(f"table {table} not in [0, 2]")
         if table == 0:
             if not self._t0_init:
-                self._initialize_table0
+                self._initialize_table0()
             qs_core = (self.table0[it2] + (ap1 - it2) * self.des0[it2]) / (
                 constants.RVGAS * temperature * density
             )
@@ -86,7 +156,8 @@ class HumiditySaturationTables:
 
         elif table == 2:
             if not self._t2_init:
-                self._initialize_table2
+                # self._initialize_table2
+                self._init_table2_qs()
             qs_core = (self.table2[it2] + (ap1 - it2) * self.des2[it2]) / (
                 constants.RVGAS * temperature * density
             )
