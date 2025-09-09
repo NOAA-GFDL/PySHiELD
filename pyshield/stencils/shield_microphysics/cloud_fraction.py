eef3121fb5a27501b5fb2b49c3099bcceaf2cf09
Oliver Elbert <oliver.elbert36@gmail.com>
2025-09-09 15:21:33 -0400

    lint

--- pyshield/stencils/shield_microphysics/cloud_fraction.py
+++ pyshield/stencils/shield_microphysics/cloud_fraction.py
@@ -5,7 +5,6 @@ from ndsl.dsl.gt4py import function as gtfunction
 from ndsl.dsl.gt4py import interval, log, log10, max, min
 from ndsl.dsl.stencil import GridIndexing, StencilFactory
 from ndsl.dsl.typing import FloatField, FloatFieldIJ
-
 from pyshield._config import MicroPhysicsConfig
 
 
