30c8b3ac4f52cfd51b956c8c434cbfbb1bb9b675
Oliver Elbert <oliver.elbert36@gmail.com>
2025-09-17 14:01:25 -0400

    oops

--- pyshield/stencils/__init__.py
+++ pyshield/stencils/__init__.py
@@ -1,11 +1,9 @@
 from .gfs_microphysics import GFSMicrophysics, GFSMicrophysicsState
-from .physics import Physics
 
 
 """
 GFSMicrophysics: GFS Cloud Microphysics class
 GFSMicrophysicsState: Class containing the state for the GFS Cloud Microphysics
-Physics: PySHiELD Physics driver
 """
 
-__all__ = ["GFSMicrophysics", "GFSMicrophysicsState", "Physics"]
+__all__ = ["GFSMicrophysics", "GFSMicrophysicsState"]
