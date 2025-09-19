63cae72813ec0513b08ef8910e81d01795e7d72d
Oliver Elbert <oliver.elbert36@gmail.com>
2025-09-19 18:00:42 -0400

    datpath

--- tests/integration/test_gfdl_cld_mp.py
+++ tests/integration/test_gfdl_cld_mp.py
@@ -104,7 +104,7 @@ def states_from_fortran_restarts(
     return state
 
 
-@pytest.mark.parametrize("restart_path", [Path("test_data/radtest/")])
+@pytest.mark.parametrize("restart_path", [Path("test_data/RESTART/")])
 @pytest.mark.parametrize("backend", ["numpy"])
 def test_gfdl_cld_mp_runs(restart_path: Path, backend: str):
     dycore_path = restart_path.joinpath("fv_core.res.tile1.nc")
