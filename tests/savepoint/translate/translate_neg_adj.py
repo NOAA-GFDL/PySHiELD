7e1f42e3c68486f7ec915a1d54ce7ec47efc32d4
Oliver Elbert <oliver.elbert36@gmail.com>
2025-11-22 00:56:54 -0500

    through particle props

--- tests/savepoint/translate/translate_neg_adj.py
+++ tests/savepoint/translate/translate_neg_adj.py
@@ -27,32 +27,101 @@ class TranslateNegAdjP(TranslatePhysicsFortranData2Py):
         self.in_vars["parameters"] = ["convt"]
 
         self.out_vars = {
-            "qvapor": {"serialname": "ne_qv", "kend": config.npz, "shield": True},
-            "qliquid": {"serialname": "ne_ql", "kend": config.npz, "shield": True},
-            "qrain": {"serialname": "ne_qr", "kend": config.npz, "shield": True},
-            "qice": {"serialname": "ne_qi", "kend": config.npz, "shield": True},
-            "qsnow": {"serialname": "ne_qs", "kend": config.npz, "shield": True},
-            "qgraupel": {"serialname": "ne_qg", "kend": config.npz, "shield": True},
-            "temperature": {
-                "serialname": "ne_pt",
-                "kend": config.npz,
+            "qvapor": {"serialname": "ne_qv", "kend": self.config.npz, "shield": True},
+            "qliquid": {"serialname": "ne_ql", "kend": self.config.npz, "shield": True},
+            "qrain": {"serialname": "ne_qr", "kend": self.config.npz, "shield": True},
+            "qice": {"serialname": "ne_qi", "kend": self.config.npz, "shield": True},
+            "qsnow": {"serialname": "ne_qs", "kend": self.config.npz, "shield": True},
+            "qgraupel": {
+                "serialname": "ne_qg",
+                "kend": self.config.npz,
                 "shield": True,
             },
-            "delp": {"serialname": "ne_delp", "kend": config.npz, "shield": True},
+            "temperature": {
+                "serialname": "ne_pt",
+                "kend": self.config.npz,
+                "shield": True,
+            },
+            "delp": {"serialname": "ne_delp", "kend": self.config.npz, "shield": True},
             "condensation": {"serialname": "ne_cond", "shield": True},
         }
 
         self.stencil_factory = stencil_factory
         self.grid_indexing = self.stencil_factory.grid_indexing
-        mpconfig = GFDLCloudMPConfig.from_config(config)
-        self.config = mpconfig.adjustnegative
+        mpconfig = GFDLCloudMPConfig(
+            dt_full=self.config.dt_atmos,
+            hydrostatic=self.config.hydrostatic,
+            npx=self.config.npx,
+            npy=self.config.npy,
+            npz=self.config.npz,
+            nwat=self.config.nwat,
+            do_qa=self.config.do_qa,
+            do_inline_mp=self.config.do_inline_mp,
+            c_cracw=self.config.c_cracw,
+            c_paut=self.config.c_paut,
+            c_pgacs=self.config.c_pgacs,
+            c_psaci=self.config.c_psaci,
+            ccn_l=self.config.ccn_l,
+            ccn_o=self.config.ccn_o,
+            const_vg=self.config.const_vg,
+            const_vi=self.config.const_vi,
+            const_vr=self.config.const_vr,
+            const_vs=self.config.const_vs,
+            vs_fac=self.config.vs_fac,
+            vg_fac=self.config.vg_fac,
+            vi_fac=self.config.vi_fac,
+            vr_fac=self.config.vr_fac,
+            de_ice=self.config.de_ice,
+            layout=self.config.layout,
+            tau_imlt=self.config.tau_imlt,
+            tau_i2s=self.config.tau_i2s,
+            tau_g2v=self.config.tau_g2v,
+            tau_v2g=self.config.tau_v2g,
+            ql_mlt=self.config.ql_mlt,
+            qs_mlt=self.config.qs_mlt,
+            t_sub=self.config.t_sub,
+            qi_gen=self.config.qi_gen,
+            qi_lim=self.config.qi_lim,
+            qi0_max=self.config.qi0_max,
+            rad_snow=self.config.rad_snow,
+            rad_rain=self.config.rad_rain,
+            dw_ocean=self.config.dw_ocean,
+            dw_land=self.config.dw_land,
+            tau_l2v=self.config.tau_l2v,
+            c2l_ord=self.config.c2l_ord,
+            do_sedi_heat=self.config.do_sedi_heat,
+            do_sedi_w=self.config.do_sedi_w,
+            fast_sat_adj=self.config.fast_sat_adj,
+            qc_crt=self.config.qc_crt,
+            fix_negative=self.config.fix_negative,
+            irain_f=self.config.irain_f,
+            mp_time=self.config.mp_time,
+            prog_ccn=self.config.prog_ccn,
+            qi0_crt=self.config.qi0_crt,
+            qs0_crt=self.config.qs0_crt,
+            rh_inc=self.config.rh_inc,
+            rh_inr=self.config.rh_inr,
+            rthresh=self.config.rthresh,
+            sedi_transport=self.config.sedi_transport,
+            use_ppm=self.config.use_ppm,
+            vg_max=self.config.vg_max,
+            vi_max=self.config.vi_max,
+            vr_max=self.config.vr_max,
+            vs_max=self.config.vs_max,
+            z_slope_ice=self.config.z_slope_ice,
+            z_slope_liq=self.config.z_slope_liq,
+            tice=self.config.tice,
+            alin=self.config.alin,
+            clin=self.config.clin,
+        )
+        self.negconfig = mpconfig.adjustnegative
 
     def compute(self, inputs):
         self.make_storage_data_input_vars(inputs)
 
         compute_func = AdjustNegativeTracers(
             self.stencil_factory,
-            self.config,
+            self.negconfig,
             convert_mm_day=inputs.pop("convt"),
         )
 
