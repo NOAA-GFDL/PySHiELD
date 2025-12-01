76cfc7b6a475b060c593376912024c30f56ff0e1
Frank Malatino <frank.malatino@noaa.gov>
2025-12-01 11:46:44 -0500

    Adding pip installable version of pyrte-rrtmgp from personal fork

--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
-import subprocess
+# import subprocess
 
 from setuptools import setup
-from setuptools.command.install import install
 
 
-class CustomInstall(install):
-    def run(self):
-        print("INSTALLING PYRTE-RRTMGP\n")
-        try:
-            result = subprocess.run(
-                ["conda", "install", "conda-forge::pyrte_rrtmgp"],
-                check=True,
-                capture_output=True,
-                text=True,
-            )
-            print("pyRTE-RRTMGP install successful\n")
-            print("STDOUT:", result.stdout)
-            print("STDERR:", result.stderr)
-        except subprocess.CalledProcessError as e:
-            print(f"Error during conda install: {e}")
-            print("STDOUT:", e.stdout)
-            print("STDERR:", e.stderr)
-        except FileNotFoundError:
-            print("Error: 'conda' command not found.")
-        install.run(self)
+# from setuptools.command.install import install
+
+
+# class CustomInstall(install):
+#     def run(self):
+#         print("INSTALLING PYRTE-RRTMGP\n")
+#         try:
+#             result = subprocess.run(
+#                 ["conda", "install", "conda-forge::pyrte_rrtmgp"],
+#                 check=True,
+#                 capture_output=True,
+#                 text=True,
+#             )
+#             print("pyRTE-RRTMGP install successful\n")
+#             print("STDOUT:", result.stdout)
+#             print("STDERR:", result.stderr)
+#         except subprocess.CalledProcessError as e:
+#             print(f"Error during conda install: {e}")
+#             print("STDOUT:", e.stdout)
+#             print("STDERR:", e.stderr)
+#         except FileNotFoundError:
+#             print("Error: 'conda' command not found.")
+#         install.run(self)
 
 
 setup(
-    cmdclass={"install": CustomInstall},
+    # cmdclass={"install": CustomInstall},
 )
